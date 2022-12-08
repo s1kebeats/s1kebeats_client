b1a07120fa858494d2741c7275f37c191048b495
datsenkoboos <adacenkoboos@gmail.com>
2022-12-08 22:49:27 +0700

    working on registration form

---
+++ components/BaseTitledInput.vue
@@ -0,0 +1,22 @@
+<template>
+    <div class="rounded-lg border-[1px] px-5 py-2">
+        <p class="text-sm">{{ title }}</p>
+        <input :type="type ? type : 'text'" :placeholder="placeholder" :value="value" @input="updateValue">
+        <slot />
+    </div>
+</template>
+<script setup lang="ts">
+const props = defineProps<{
+    title: string;
+    placeholder?: string;
+    value: string;
+    type?: string;
+}>()
+const emit = defineEmits<{
+    (e: "updateValue", value: string): void
+}>()
+const updateValue = (e: Event) => {
+    const input = e.target as HTMLInputElement;
+    emit("updateValue", input.value);
+}
+</script>
--- components/TheRegistrationForm.vue
+++ components/TheRegistrationForm.vue
@@ -1,10 +1,18 @@
 <template>
-    <form @submit.prevent="register" class="flex flex-col w-[500px]">
-        <input type="text" v-model="registrationState.username">
-        <input type="text" v-model="registrationState.email">
-        <input type="text" v-model="registrationState.password">
-        <input type="text" v-model="registrationState.passwordConfirm">
-        <button type="submit" class="">Зарегистрироваться</button>
+    <form @submit.prevent="register" class="flex flex-col gap-5 w-[500px] bg-white rounded-lg px-10 py-5">
+        <div class="flex flex-col gap-1">
+            <h1 class="text-2xl font-semibold">Регистрация</h1>
+            <h2 class="text-sm">Уже есть аккаунт? <nuxt-link to="/login" class="text-[#7945fc] hover:font-semibold transition-all">Войдите</nuxt-link></h2>
+        </div>
+        <BaseTitledInput title="Имя пользователя" placeholder="Введите имя пользователя" :value="registrationState.username" />
+        <BaseTitledInput type="email" title="Электронная почта" placeholder="Введите электронную почту" :value="registrationState.email" />
+        <BaseTitledInput type="password" title="Пароль" placeholder="Введите имя пароль" :value="registrationState.password">
+            <!-- <SeePasswordButton /> -->
+        </BaseTitledInput>
+        <BaseTitledInput type="password" title="Подтверждение пароля" placeholder="Введите пароль ещё раз" :value="registrationState.passwordConfirm">
+            <!-- <SeePasswordButton /> -->
+        </BaseTitledInput>
+        <button type="submit" class="bg-[#7945fc] text-white rounded-lg py-2 text-sm">Зарегистрироваться</button>
         <span v-for="error in v$.$errors" :key="error.$uid">
             {{ error.$property }} - {{  error.$message }}
         </span>
