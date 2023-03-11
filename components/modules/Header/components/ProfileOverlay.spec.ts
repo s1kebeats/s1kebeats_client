8806bfec7ddead0b502501973c3406737e628ec5
datsenkoboos <adacenkoboos@gmail.com>
2023-03-11 20:18:06 +0700

    profile overlay tests done

---
+++ components/modules/Header/components/ProfileOverlay.spec.ts
@@ -0,0 +1,104 @@
+import ProfileOverlay from './ProfileOverlay.vue'
+import { describe, expect, it } from 'vitest';
+import { mount } from '@vue/test-utils';
+import { createTestingPinia } from '@pinia/testing';
+import User from '@/api/models/User';
+
+const profileOverlaySelector = '[data-testid=profileOverlay]'
+const loginButtonSelector = '[data-testid=loginButton]'
+const logoutButtonSelector = '[data-testid=logoutButton]'
+const profileLinkSelector = '[data-testid=profileLink]'
+const likedLinkSelector = '[data-testid=likedLink]'
+
+const testUser: User = {
+    email: 'test@example.com',
+    username: 's1kebeats',
+    id: 777,
+    image: 'path/to/Image.png',
+    displayedName: null,
+};
+
+describe('ProfileOverlay', () => {
+  it('is not visible with uiStore.profileOverlay set to false', async () => {
+    const wrapper = mount(ProfileOverlay, {
+      global: {
+        plugins: [
+          createTestingPinia({
+            initialState: {
+              ui: {
+                profileOverlay: false
+              }
+            },
+          }),
+        ],
+      },
+    });
+
+    expect(wrapper.get(profileOverlaySelector).isVisible()).toBe(false);
+  });
+  it('is visible with uiStore.profileOverlay set to true', async () => {
+    const wrapper = mount(ProfileOverlay, {
+      global: {
+        plugins: [
+          createTestingPinia({
+            initialState: {
+              ui: {
+                profileOverlay: true
+              }
+            },
+          }),
+        ],
+      },
+    });
+
+    expect(wrapper.get(profileOverlaySelector).isVisible()).toBe(true);
+  });
+  it('renders login button when not authorized', async () => {
+    const wrapper = mount(ProfileOverlay, {
+      global: {
+        plugins: [
+          createTestingPinia({
+            initialState: {
+                auth: {
+                    authorized: false,
+                    user: null,
+                },
+              ui: {
+                profileOverlay: true
+              }
+            },
+          }),
+        ],
+      },
+    });
+
+    expect(wrapper.find(loginButtonSelector).exists()).toBe(true);
+    expect(wrapper.find(logoutButtonSelector).exists()).toBe(false);
+    expect(wrapper.find(profileLinkSelector).exists()).toBe(false);
+    expect(wrapper.find(likedLinkSelector).exists()).toBe(false);
+  });
+  it('renders logout button and links when authorized', async () => {
+    const wrapper = mount(ProfileOverlay, {
+      global: {
+        plugins: [
+          createTestingPinia({
+            initialState: {
+                auth: {
+                    authorized: true,
+                    user: testUser,
+                },
+              ui: {
+                profileOverlay: true
+              }
+            },
+          }),
+        ],
+      },
+    });
+
+    expect(wrapper.find(loginButtonSelector).exists()).toBe(false);
+    expect(wrapper.find(logoutButtonSelector).exists()).toBe(true);
+    expect(wrapper.find(profileLinkSelector).exists()).toBe(true);
+    expect(wrapper.find(likedLinkSelector).exists()).toBe(true);
+  });
+});
--- components/modules/Header/components/ProfileOverlay.vue
+++ components/modules/Header/components/ProfileOverlay.vue
@@ -1,10 +1,12 @@
 <template>
   <div
+    data-testid="profileOverlay"
     class="shadow select-none absolute z-[9999] right-0 top-11 bg-white rounded-md flex flex-col items-center gap-2 px-4 py-3"
     v-show="uiStore.profileOverlay"
   >
     <template v-if="authStore.authorized">
       <nuxt-link
+        data-testid="profileLink"
         :to="`/${authStore.user!.username}`"
         class="w-full text-right text-sm text-black flex items-center justify-start gap-3 transition-all !hover:text-[#7945fc]"
       >
@@ -14,15 +16,17 @@
       <nuxt-link
         to="/liked"
         class="w-full text-right text-sm text-black flex items-center justify-start gap-3 transition-all !hover:text-[#7945fc]"
+        data-testid="likedLink"
       >
         <Icon name="icon-park-outline:like" size="18px" />
         <span> Понравившееся </span>
       </nuxt-link>
-      <LogoutButton />
+      <LogoutButton data-testid="logoutButton" />
     </template>
     <nuxt-link
       v-else
       to="/login"
+      data-testid="loginButton"
       class="w-full text-right text-sm font-medium flex items-center justify-start gap-3"
     >
       <Icon name="material-symbols:login-rounded" size="16px" />
