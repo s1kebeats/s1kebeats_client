4287807ccf84f9e18360ec0bbc7fa1e1a32c1a9c
datsenkoboos <adacenkoboos@gmail.com>
2023-05-06 23:20:32 +0700

    testing everything, gotta mock nuxt runtimeconfig and test apiImage

--- components/ui/ApiImage.spec.ts
+++ components/ui/ApiImage.spec.ts
@@ -3,18 +3,29 @@ import { describe, it, expect } from 'vitest';
 import { mount } from '@vue/test-utils';
 
 const apiImageSelector = '[data-testid=apiImage]';
+// TODO: MEDIA_URL from Nuxt runtimeConfig
+const MEDIA_URL = 'http://192.168.1.135:5000/api/media/';
 
 describe('ApiImage', () => {
-  it('renders with api-based src', () => {
-    const testSrc = 'test';
-    const wrapper = mount(ApiImage, {
-      props: {
-        src: testSrc,
-      },
-    });
+  describe('props', () => {
+    it('src - should render with api-based src', () => {
+      const testSrc = 'test';
+      const wrapper = mount(ApiImage, {
+        props: {
+          src: testSrc,
+        },
+        mocks: {
+          useRuntimeConfig: () => ({
+            public: {
+              MEDIA_URL,
+            },
+          }),
+        },
+      });
 
-    expect(wrapper.get(apiImageSelector).attributes('src')).toBe(
-      `http://localhost:5000/api/media/${testSrc}`
-    );
+      expect(wrapper.get(apiImageSelector).attributes('src')).toBe(
+        MEDIA_URL + testSrc
+      );
+    });
   });
 });
