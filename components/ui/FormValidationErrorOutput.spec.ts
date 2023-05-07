f2724f6d9772599433168b1b4c91b135a4760f65
datsenkoboos <adacenkoboos@gmail.com>
2023-05-07 22:20:55 +0700

    form validation error output test done

--- components/ui/FormValidationErrorOutput.spec.ts
+++ components/ui/FormValidationErrorOutput.spec.ts
@@ -6,48 +6,50 @@ import { type Validation } from '@vuelidate/core';
 const errorListSelector = '[data-testid=errorList]';
 
 describe('FormValidationErrorOutput', async () => {
-  it('should render invisible without error list', () => {
-    const wrapper = mount(FormValidationErrorOutput, {
-      props: {
-        v: {
-          $errors: [],
-        } as unknown as Validation,
-      },
+  describe('props', () => {
+    it('v - should not render with empty error list', () => {
+      const wrapper = mount(FormValidationErrorOutput, {
+        props: {
+          v: {
+            $errors: [],
+          } as unknown as Validation,
+        },
+      });
+  
+      expect(wrapper.find(errorListSelector).exists()).toBe(false);
     });
-
-    expect(wrapper.find(errorListSelector).exists()).toBe(false);
-  });
-  it('should render visible with error list', () => {
-    const wrapper = mount(FormValidationErrorOutput, {
-      props: {
-        v: {
-          $errors: [
-            {
-              $message: 'error',
-            },
-          ],
-        } as Validation,
-      },
+    it('v - should render with populated error list', () => {
+      const wrapper = mount(FormValidationErrorOutput, {
+        props: {
+          v: {
+            $errors: [
+              {
+                $message: 'error',
+              },
+            ],
+          } as Validation,
+        },
+      });
+  
+      expect(wrapper.find(errorListSelector).exists()).toBe(true);
     });
-
-    expect(wrapper.find(errorListSelector).exists()).toBe(true);
-  });
-  it('should render first error message', () => {
-    const wrapper = mount(FormValidationErrorOutput, {
-      props: {
-        v: {
-          $errors: [
-            {
-              $message: 'first',
-            },
-            {
-              $message: 'second',
-            },
-          ],
-        } as Validation,
-      },
+    it('v- should render message from error list', () => {
+      const wrapper = mount(FormValidationErrorOutput, {
+        props: {
+          v: {
+            $errors: [
+              {
+                $message: 'first',
+              },
+              {
+                $message: 'second',
+              },
+            ],
+          } as Validation,
+        },
+      });
+  
+      expect(wrapper.find(errorListSelector).text()).toBe('first');
     });
-
-    expect(wrapper.find(errorListSelector).text()).toBe('first');
-  });
+  })
 });
