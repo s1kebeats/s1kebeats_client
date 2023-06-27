ab6a3b272087a56f7398b0fd0f79cf86e687d3a0
datsenkoboos <adacenkoboos@gmail.com>
2023-06-27 11:38:13 +0700

    email activation error tests done

--- components/modules/EmailActivation/components/EmailActivationError.spec.ts
+++ components/modules/EmailActivation/components/EmailActivationError.spec.ts
@@ -8,47 +8,97 @@ const errorTitleSelector = '[data-testid=errorTitle]';
 const errorDescriptionSelector = '[data-testid=errorDescription]';
 
 describe('EmailActivationError', () => {
-  it('is not visible when emailActivationStore.error.state is false', async () => {
-    const wrapper = shallowMount(EmailActivationError, {
-      global: {
-        plugins: [
-          createTestingPinia({
-            initialState: {
-              emailActivation: {
-                error: {
-                  state: false,
-                  status: null,
+  describe('emailActivation store', () => {
+    it('error.state - should not render when set to false', () => {
+      const wrapper = shallowMount(EmailActivationError, {
+        global: {
+          plugins: [
+            createTestingPinia({
+              initialState: {
+                emailActivation: {
+                  error: {
+                    state: false,
+                    status: null,
+                  },
                 },
               },
-            },
-          }),
-        ],
-      },
-    });
+            }),
+          ],
+        },
+      });
 
-    expect(wrapper.find(emailActivationErrorSelector).exists()).toBeFalsy();
-  });
-  it('is visible when emailActivationStore.error.state is true', async () => {
-    const wrapper = shallowMount(EmailActivationError, {
-      global: {
-        plugins: [
-          createTestingPinia({
-            initialState: {
-              emailActivation: {
-                error: {
-                  state: true,
-                  status: null,
+      expect(wrapper.find(emailActivationErrorSelector).exists()).toBe(false);
+    });
+    it('error.state - should render when set to true', () => {
+      const wrapper = shallowMount(EmailActivationError, {
+        global: {
+          plugins: [
+            createTestingPinia({
+              initialState: {
+                emailActivation: {
+                  error: {
+                    state: true,
+                    status: 404,
+                  },
                 },
               },
-            },
-          }),
-        ],
-      },
-    });
+            }),
+          ],
+        },
+      });
 
-    expect(wrapper.find(emailActivationErrorSelector).exists()).toBe(true);
+      expect(wrapper.find(emailActivationErrorSelector).exists()).toBe(true);
+    });
+    it('error.status - should render check link error when set to 404', async () => {
+      const wrapper = shallowMount(EmailActivationError, {
+        global: {
+          plugins: [
+            createTestingPinia({
+              initialState: {
+                emailActivation: {
+                  error: {
+                    state: true,
+                    status: 404,
+                  },
+                },
+              },
+            }),
+          ],
+        },
+      });
+
+      expect(wrapper.get(errorTitleSelector).text()).toBe('Ошибка');
+      expect(wrapper.get(errorDescriptionSelector).text()).toBe(
+        'Проверьте ссылку'
+      );
+    });
+    it('error.status - should render unexpexted error when seto to anything except 404', async () => {
+      const wrapper = shallowMount(EmailActivationError, {
+        global: {
+          plugins: [
+            createTestingPinia({
+              initialState: {
+                emailActivation: {
+                  error: {
+                    state: true,
+                    status: 500,
+                  },
+                },
+              },
+            }),
+          ],
+        },
+      });
+
+      expect(wrapper.get(errorTitleSelector).text()).toBe(
+        'Произошла непредвиденная ошибка'
+      );
+      expect(wrapper.get(errorDescriptionSelector).text()).toBe(
+        'Проверьте ваше интернет соединение'
+      );
+    });
   });
-  it('should render check link error when emailActivationStore.error.status is 404', async () => {
+  it('snapshot - should match the shapshot', () => {
     const wrapper = shallowMount(EmailActivationError, {
       global: {
         plugins: [
@@ -65,35 +115,36 @@ describe('EmailActivationError', () => {
         ],
       },
     });
-
-    expect(wrapper.get(errorTitleSelector).text()).toBe('Ошибка');
-    expect(wrapper.get(errorDescriptionSelector).text()).toBe(
-      'Проверьте ссылку'
-    );
-  });
-  it('should render unexpexted error when emailActivationStore.error.status is not 404', async () => {
-    const wrapper = shallowMount(EmailActivationError, {
-      global: {
-        plugins: [
-          createTestingPinia({
-            initialState: {
-              emailActivation: {
-                error: {
-                  state: true,
-                  status: 500,
-                },
-              },
-            },
-          }),
-        ],
-      },
-    });
-
-    expect(wrapper.get(errorTitleSelector).text()).toBe(
-      'Произошла непредвиденная ошибка'
-    );
-    expect(wrapper.get(errorDescriptionSelector).text()).toBe(
-      'Проверьте ваше интернет соединение'
-    );
+    expect(wrapper.get(emailActivationErrorSelector)).toMatchInlineSnapshot(`
+      DOMWrapper {
+        "isDisabled": [Function],
+        "wrapperElement": <div
+          class="flex flex-col gap-1 items-center justify-center"
+          data-testid="emailActivationError"
+        >
+          <icon
+            color="#ff0000"
+            name="material-symbols:warning-rounded"
+            size="90px"
+          />
+          <div
+            class="text-center flex flex-col gap-1"
+          >
+            <p
+              class="text-md font-semibold"
+              data-testid="errorTitle"
+            >
+              Ошибка
+            </p>
+            <p
+              class="text-xs"
+              data-testid="errorDescription"
+            >
+              Проверьте ссылку
+            </p>
+          </div>
+        </div>,
+      }
+    `);
   });
 });
