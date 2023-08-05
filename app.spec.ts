402fbb45365ceb7ed7b7c9d2b9d36a9f5fd456e4
datsenkoboos <adacenkoboos@gmail.com>
2023-08-05 18:08:35 +0700

    added app.vue snapshot test

---
+++ app.spec.ts
@@ -0,0 +1,21 @@
+import app from './app.vue'
+import { describe, it, expect } from 'vitest'
+import { shallowMount } from '@vue/test-utils'
+
+describe('app', () => {
+    it('snapshot - should match the snapshot', () => {
+        const wrapper = shallowMount(app)
+        expect(wrapper.element).toMatchInlineSnapshot(`
+          <div
+            data-v-app=""
+          >
+            
+            <loading-screen-stub />
+            <nuxtlayout>
+              <nuxtpage />
+            </nuxtlayout>
+            
+          </div>
+        `)
+    })
+})
