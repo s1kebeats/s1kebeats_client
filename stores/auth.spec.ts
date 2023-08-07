b0f3ccd307a6bbfdd717b9125f298ae0968838a5
datsenkoboos <adacenkoboos@gmail.com>
2023-08-07 20:59:02 +0700

    authStore perfect

---
+++ stores/auth.spec.ts
@@ -0,0 +1,285 @@
+import useAuthStore from './auth';
+import { setActivePinia, createPinia } from 'pinia';
+import {
+  Mock,
+  beforeAll,
+  beforeEach,
+  describe,
+  expect,
+  it,
+  test,
+  vi,
+} from 'vitest';
+import type User from '@/api/models/User';
+import { authResponseMock } from './api/mocks';
+import login from './api/login';
+import logout from './api/logout';
+import refresh from './api/refresh';
+import showUnexpectedError from '@/plugins/showUnexpectedError';
+import activate from './api/activate';
+
+vi.mock('@/plugins/showUnexpectedError');
+
+const testError = () => new Error('Api error');
+
+const testUser: User = {
+  id: 1,
+  email: 'test@example.com',
+  username: 'test',
+  image: null,
+  displayedName: null,
+};
+
+vi.mock('./api/logout');
+vi.mock('./api/activate');
+vi.mock('./api/login', () => {
+  return {
+    default: vi.fn(() => {
+      return {
+        data: authResponseMock,
+      };
+    }),
+  };
+});
+vi.mock('./api/refresh', () => {
+  return {
+    default: vi.fn(() => {
+      return {
+        data: authResponseMock,
+      };
+    }),
+  };
+});
+
+describe('Auth Store', () => {
+  beforeEach(() => {
+    setActivePinia(createPinia());
+    vi.clearAllMocks();
+  });
+  describe('state', () => {
+    test('authorized - should be null', () => {
+      const store = useAuthStore();
+      expect(store.authorized).toBeNull();
+    });
+    test('user - should be null', () => {
+      const store = useAuthStore();
+      expect(store.user).toBeNull();
+    });
+  });
+  describe('actions', () => {
+    test('setUser - should set store.user to provided value', () => {
+      const store = useAuthStore();
+      store.setUser(testUser);
+      expect(store.user).toStrictEqual(testUser);
+      store.setUser(null);
+      expect(store.user).toBeNull();
+    });
+    test('setAuthorized - should set store.authorized to provided value', () => {
+      const store = useAuthStore();
+      store.setAuthorized(true);
+      expect(store.authorized).toBe(true);
+      store.setAuthorized(false);
+      expect(store.authorized).toBe(false);
+    });
+    describe('activate', () => {
+      it('should call activate function with passed activationCode', async () => {
+        const testCode = 'testCode';
+        const store = useAuthStore();
+
+        await store.activate(testCode);
+        expect(activate).toHaveBeenCalled()
+        expect(activate).toHaveBeenCalledWith(testCode)
+      })
+      describe('activate - error', () => {
+        beforeAll(() => {
+          (activate as Mock).mockRejectedValue(testError());
+        });
+        it('should throw error', async () => {
+          const store = useAuthStore();
+          expect(() =>
+            store.activate('code')
+          ).rejects.toThrowError();
+        });
+        it('should throw error returned from activate', async () => {
+          const store = useAuthStore();
+          try {
+            await store.activate('code');
+          } catch (error) {
+            expect(error).toStrictEqual(testError());
+          }
+        });
+      })
+    })
+    describe('login', () => {
+      it('should call login function with passed params', async () => {
+        const testData = {
+          username: 'username',
+          password: 'password',
+          rememberMe: true,
+        };
+        const store = useAuthStore();
+
+        await store.login(
+          testData.username,
+          testData.password,
+          testData.rememberMe
+        );
+        expect(login).toHaveBeenCalled();
+        expect(login).toHaveBeenCalledWith(
+          testData.username,
+          testData.password,
+          testData.rememberMe
+        );
+      });
+      describe('login - success', () => {
+        beforeAll(() => {
+          (login as Mock).mockImplementation(() => {
+            return {
+              data: authResponseMock,
+            };
+          });
+        });
+        it('should set store.user to data received from login', async () => {
+          const store = useAuthStore();
+
+          await store.login('username', 'password', true);
+          expect(store.user).toStrictEqual(authResponseMock.user);
+        });
+        it('should set store.authorized to "true"', async () => {
+          const store = useAuthStore();
+          store.authorized = false;
+
+          await store.login('username', 'password', true);
+          expect(store.authorized).toBe(true);
+        });
+      });
+      describe('login - error', () => {
+        beforeAll(() => {
+          (login as Mock).mockRejectedValue(testError());
+        });
+        it('should throw error', async () => {
+          const store = useAuthStore();
+          expect(() =>
+            store.login('username', 'password', true)
+          ).rejects.toThrowError();
+        });
+        it('should throw error returned from login', async () => {
+          const store = useAuthStore();
+          try {
+            await store.login('username', 'password', true);
+          } catch (error) {
+            expect(error).toStrictEqual(testError());
+          }
+        });
+        it('should set store.authorized to "false"', async () => {
+          const store = useAuthStore();
+          store.authorized = true;
+          try {
+            await store.login('username', 'password', true);
+          } catch (error) {
+            expect(store.authorized).toBe(false);
+          }
+        });
+      });
+    });
+    describe('logout', () => {
+      it('should call logout function', async () => {
+        const store = useAuthStore();
+
+        await store.logout();
+        expect(logout).toHaveBeenCalled();
+      });
+      describe('logout - success', () => {
+        it('should set store.user to "null"', async () => {
+          const store = useAuthStore();
+          store.user = {} as User;
+
+          await store.logout();
+          expect(store.user).toBeNull();
+        });
+        it('should set store.authorized to "false"', async () => {
+          const store = useAuthStore();
+          store.authorized = true;
+
+          await store.logout();
+          expect(store.authorized).toBe(false);
+        });
+      });
+      describe('logout - error', () => {
+        beforeAll(() => {
+          (logout as Mock).mockRejectedValue(testError());
+        });
+        it('should call showUnexpectedError with error from logout()', async () => {
+          const store = useAuthStore();
+          await store.logout();
+          expect(showUnexpectedError).toHaveBeenCalled();
+          expect(showUnexpectedError).toHaveBeenCalledWith(testError());
+        });
+      });
+    });
+    describe('checkAuth', () => {
+      it('should call refresh function', async () => {
+        const store = useAuthStore();
+
+        await store.checkAuth();
+        expect(refresh).toHaveBeenCalled();
+      });
+      describe('refresh - success', () => {
+        beforeAll(() => {
+          (refresh as Mock).mockImplementation(() => {
+            return {
+              data: authResponseMock,
+            };
+          });
+        });
+        it('should set store.user to data received from refresh', async () => {
+          const store = useAuthStore();
+
+          await store.checkAuth();
+          expect(store.user).toStrictEqual(authResponseMock.user);
+        });
+        it('should set store.authorized to "true"', async () => {
+          const store = useAuthStore();
+
+          await store.checkAuth();
+          expect(store.authorized).toBe(true);
+        });
+      });
+      describe('refresh - error', () => {
+        beforeAll(() => {
+          (refresh as Mock).mockRejectedValue(testError());
+        });
+        it('should set store.user to "null"', async () => {
+          const store = useAuthStore();
+          store.user = {} as User;
+
+          await store.checkAuth();
+          expect(store.user).toBeNull();
+        });
+        it('should set store.authorized to "false"', async () => {
+          const store = useAuthStore();
+          store.authorized = true;
+
+          await store.checkAuth();
+          expect(store.authorized).toBe(false);
+        });
+        it('should call showUnexpectedError with error from refresh() when error.response.status is not 401', async () => {
+          const store = useAuthStore();
+          await store.checkAuth();
+          expect(showUnexpectedError).toHaveBeenCalled();
+          expect(showUnexpectedError).toHaveBeenCalledWith(testError());
+        });
+        it('should not call showUnexpectedError when error.response.status is 401', async () => {
+          const store = useAuthStore();
+          (refresh as Mock).mockRejectedValueOnce({
+            response: {
+              status: 401,
+            },
+          });
+          await store.checkAuth();
+          expect(showUnexpectedError).not.toHaveBeenCalled();
+        });
+      });
+    });
+  });
+});
---
+++ stores/auth.ts
@@ -0,0 +1,76 @@
+import { defineStore } from 'pinia';
+import refresh from './api/refresh';
+import type User from '@/api/models/User';
+import logout from './api/logout';
+import login from './api/login';
+import activate from './api/activate';
+import showUnexpectedError from '@/plugins/showUnexpectedError';
+
+const useAuthStore = defineStore('auth', {
+  state: (): {
+    authorized: boolean | null;
+    user: User | null;
+  } => {
+    return {
+      authorized: null,
+      user: null,
+    };
+  },
+  actions: {
+    setUser(user: User | null): void {
+      if (this.user != null && user != null) {
+        Object.assign(this.user, user);
+      } else {
+        this.user = user;
+      }
+    },
+    setAuthorized(authorized: boolean): void {
+      this.authorized = authorized;
+    },
+    async checkAuth(): Promise<void> {
+      try {
+        const { data } = await refresh();
+        this.setUser(data.user);
+        this.setAuthorized(true);
+      } catch (error: any) {
+        this.setUser(null);
+        this.setAuthorized(false);
+        if (error.response?.status !== 401) {
+          showUnexpectedError(error);
+        }
+      }
+    },
+    async logout(): Promise<void> {
+      try {
+        await logout();
+        this.setUser(null);
+        this.setAuthorized(false);
+      } catch (error) {
+        showUnexpectedError(error);
+      }
+    },
+    async login(
+      username: string,
+      password: string,
+      refresh: boolean
+    ): Promise<void> {
+      try {
+        const { data } = await login(username, password, refresh);
+        this.setUser(data.user);
+        this.setAuthorized(true);
+      } catch (error) {
+        this.setAuthorized(false);
+        throw error;
+      }
+    },
+    async activate(activationCode: string): Promise<void> {
+      try {
+        await activate(activationCode);
+      } catch (error) {
+        throw error;
+      }
+    },
+  },
+});
+
+export default useAuthStore;
