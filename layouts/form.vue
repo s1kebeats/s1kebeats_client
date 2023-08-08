ff9ee9ac3a290fafba35ad26ce33cdaba21e4d5b
datsenkoboos <adacenkoboos@gmail.com>
2023-08-08 18:52:47 +0700

    form layout perfect

--- layouts/form.vue
+++ layouts/form.vue
@@ -1,5 +1,19 @@
 <template>
-  <div id="form">
-    <slot />
+  <div id="layout" class="flex">
+    <section
+      class="hidden md:flex items-center justify-center grow bg-image-main"
+    >
+      <nuxt-link class="w-[min(50%,400px)]" to="/">
+        <MainLogo />
+      </nuxt-link>
+    </section>
+    <section
+      class="w-full md:w-[clamp(320px,30%,576px)] flex items-center justify-center"
+    >
+      <slot />
+    </section>
   </div>
 </template>
+<script setup lang="ts">
+import MainLogo from '@/components/ui/MainLogo/MainLogo.vue';
+</script>
