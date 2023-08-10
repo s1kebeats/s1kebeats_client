1a185d5f3f68d8d07c194cc0173639c1437ff7ef
datsenkoboos <adacenkoboos@gmail.com>
2023-08-10 10:35:18 +0700

    added register page

---
+++ pages/register.vue
@@ -0,0 +1,10 @@
+<template>
+    <RegistrationForm />
+</template>
+<script setup lang="ts">
+import RegistrationForm from '@/components/modules/RegistrationForm'
+definePageMeta({
+  layout: 'form',
+  middleware: [ 'guest-only' ]
+});
+</script>
