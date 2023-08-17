8de49c47a2d8bd5888f7e7e5b0a85446e9a05765
datsenkoboos <adacenkoboos@gmail.com>
2023-08-17 17:36:12 +0700

    updated nuxt sb config

--- vite.config.ts
+++ vite.config.ts
@@ -1,36 +1,23 @@
-import { URL, fileURLToPath } from 'node:url';
 import { defineConfig } from 'vite';
-import AutoImport from 'unplugin-auto-import/vite';
-import Components from 'unplugin-vue-components/vite';
 import vue from '@vitejs/plugin-vue';
+import { URL, fileURLToPath } from 'node:url';
+import AutoImport from 'unplugin-auto-import/vite';
 
-// https://vitejs.dev/config/
 export default defineConfig({
   plugins: [
     AutoImport({
       imports: ['vue', 'vue-router'],
-      dirs: ['./composables'],
       vueTemplate: true,
     }),
-    // Components({
-    //   dirs: [
-    //     './components/',
-    //     // Component folders that should be auto-imported
-    //   ],
-    //   dts: true,
-    //   directoryAsNamespace: true,
-    // }),
     vue(),
   ],
+  test: {
+    globals: true,
+    environment: 'happy-dom',
+  },
   resolve: {
     alias: {
       '@': fileURLToPath(new URL('./', import.meta.url)),
-      // Add any other aliases you use in your code base
-      // https://nuxt.com/docs/api/configuration/nuxt-config/#alias
     },
-  },
-  test: {
-    environment: 'happy-dom',
-    globals: true,
   },
 });
