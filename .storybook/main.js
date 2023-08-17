5550e3caa30c6d0784ab8cbf5f8bc71f0eb6c85a
datsenkoboos <adacenkoboos@gmail.com>
2023-08-17 15:55:22 +0700

    storybook addon nuxt added and fixed

--- .storybook/main.js
+++ .storybook/main.js
@@ -4,15 +4,11 @@ export default {
   addons: [
     '@storybook/addon-links',
     '@storybook/addon-essentials',
+    'storybook-addon-nuxt',
     '@storybook/addon-interactions',
   ],
   framework: {
     name: '@storybook/vue3-vite',
-    options: {
-      builder: {
-        viteConfigPath: './vite.config.ts',
-      },
-    },
   },
   docs: {
     autodocs: 'tag',
