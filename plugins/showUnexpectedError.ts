04790f1a48c26a8ed0eb48cb114b559f4fcca834
datsenkoboos <adacenkoboos@gmail.com>
2023-08-18 12:34:06 +0700

    removed redundant files

--- plugins/showUnexpectedError.ts
+++
@@ -1,9 +0,0 @@
-const unexpectedErrorMessage =
-  'Произошла непредвиденная ошибка, пожалуйста перезагрузите страницу.';
-export { unexpectedErrorMessage };
-export default function (error: any) {
-  showError({
-    statusCode: error.response?.status ? error.response.status : 500,
-    message: unexpectedErrorMessage,
-  });
-}
