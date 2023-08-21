b4f4cafdb72ebc183aaff27f1cb317e06cc61215
datsenkoboos <adacenkoboos@gmail.com>
2023-08-21 17:46:40 +0700

    format

--- api/models/responseBodies/index.ts
+++ api/models/responseBodies/index.ts
@@ -1,5 +1,11 @@
 import type RegisterResponseBody from './RegisterResponseBody';
 import type AuthResponseBody from './AuthResponseBody';
 import type ActivateResponseBody from './ActivateResponseBody';
+import type LogoutResponseBody from './LogoutResponseBody';
 
-export { RegisterResponseBody, AuthResponseBody, ActivateResponseBody };
+export {
+  RegisterResponseBody,
+  AuthResponseBody,
+  ActivateResponseBody,
+  LogoutResponseBody,
+};
