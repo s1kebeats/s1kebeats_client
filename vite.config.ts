6f4246794dc5dc840f69f8f018e27390433eb57b
datsenkoboos <adacenkoboos@gmail.com>
2023-08-21 13:55:45 +0700

    changed order in vitest cfg

--- vite.config.ts
+++ vite.config.ts
@@ -4,6 +4,11 @@ import { URL, fileURLToPath } from 'node:url';
 import AutoImport from 'unplugin-auto-import/vite';
 
 export default defineConfig({
+  resolve: {
+    alias: {
+      '@': fileURLToPath(new URL('./', import.meta.url)),
+    },
+  },
   plugins: [
     AutoImport({
       imports: ['vue', 'vue-router'],
