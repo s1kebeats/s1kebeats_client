d25c1fd7ab61ff6963251a46b54e3db2f31e6988
datsenkoboos <adacenkoboos@gmail.com>
2023-08-21 16:53:09 +0700

    format

--- stores/api/refresh.ts
+++ stores/api/refresh.ts
@@ -1,7 +1,9 @@
 import axios, { type AxiosResponse } from 'axios';
 import { type AuthResponseBody } from '@/api/models/responseBodies';
 
-export default async function refresh(): Promise<AxiosResponse<AuthResponseBody>> {
+export default async function refresh(): Promise<
+  AxiosResponse<AuthResponseBody>
+> {
   const response = await axios.post<AuthResponseBody>(
     `${process.env.API_URL}/refresh`,
     null,
