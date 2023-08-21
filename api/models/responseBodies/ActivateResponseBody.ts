2d2278e71d1a145c49e20b63528b0c2604abc5ef
datsenkoboos <adacenkoboos@gmail.com>
2023-08-21 17:10:02 +0700

    added activate response body

---
+++ api/models/responseBodies/ActivateResponseBody.ts
@@ -0,0 +1,3 @@
+type ActivateResponseBody = 'success';
+
+export default ActivateResponseBody;
--- api/models/responseBodies/index.ts
+++ api/models/responseBodies/index.ts
@@ -1,4 +1,5 @@
 import type RegisterResponseBody from './RegisterResponseBody';
 import type AuthResponseBody from './AuthResponseBody';
+import type ActivateResponseBody from './ActivateResponseBody';
 
-export { RegisterResponseBody, AuthResponseBody };
+export { RegisterResponseBody, AuthResponseBody, ActivateResponseBody };
