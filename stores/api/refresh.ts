dba11bbe486ad394339ce5c5a2165a0c07e40d64
datsenkoboos <adacenkoboos@gmail.com>
2023-08-21 16:32:14 +0700

    updated typings for refresh api call

--- stores/api/refresh.ts
+++ stores/api/refresh.ts
@@ -1,9 +1,9 @@
-import axios from 'axios';
-import type AuthResponse from '@/api/models/AuthResponse';
+import axios, { type AxiosResponse } from 'axios';
+import { type AuthResponseBody } from '@/api/models/responseBodies';
 
-export default async function refresh() {
+export default async function refresh(): Promise<AxiosResponse<AuthResponseBody>> {
   const runtimeConfig = useRuntimeConfig();
-  const response = await axios.post<AuthResponse>(
+  const response = await axios.post<AuthResponseBody>(
     `${runtimeConfig.public.API_URL}/refresh`,
     null,
     {
