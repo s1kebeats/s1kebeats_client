1330fcc1463930b1903dcb274650895c2b084d1f
datsenkoboos <adacenkoboos@gmail.com>
2023-08-22 10:22:13 +0700

    updated imports

--- mocks/responses/index.ts
+++ mocks/responses/index.ts
@@ -1,5 +1,6 @@
 import AuthResponseMock from './AuthResponseMock';
 import ActivateResponseMock from './ActivateResponseMock';
 import LogoutResponseMock from './LogoutResponseMock';
+import RegisterResponseMock from './RegisterResponseMock';
 
-export { AuthResponseMock, ActivateResponseMock, LogoutResponseMock };
+export { AuthResponseMock, ActivateResponseMock, LogoutResponseMock, RegisterResponseMock };
