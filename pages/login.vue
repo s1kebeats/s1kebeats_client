ad147d54f24ed6634f19502e866734c21d5fe2d6
datsenkoboos <adacenkoboos@gmail.com>
2023-08-24 19:28:03 +0700

    added login page

---
+++ pages/login.vue
@@ -0,0 +1,10 @@
+<template>
+  <LoginForm />
+</template>
+<script setup lang="ts">
+import LoginForm from '@/components/modules/LoginForm';
+definePageMeta({
+  layout: 'form',
+  middleware: ['guest-only'],
+});
+</script>
