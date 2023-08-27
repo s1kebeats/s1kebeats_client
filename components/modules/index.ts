c8c14bf4d6eaa894b3f27eb7755d595c3cd2962a
datsenkoboos <adacenkoboos@gmail.com>
2023-08-27 22:08:40 +0700

    AppHeader import

--- components/modules/index.ts
+++ components/modules/index.ts
@@ -1,4 +1,5 @@
 import LoginForm from './LoginForm';
 import RegistrationForm from './RegistrationForm';
+import AppHeader from './AppHeader';
 
-export { LoginForm, RegistrationForm };
+export { LoginForm, RegistrationForm, AppHeader };
