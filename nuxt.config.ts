ef518123f75c80e2bafd2d659682d6597d6f879d
datsenkoboos <adacenkoboos@gmail.com>
2023-09-08 13:10:35 +0700

    updated api_url to use with mobile network

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -1,4 +1,7 @@
-export const API_URL = 'http://192.168.1.135:5000/api';
+// exposed url for home network
+// export const API_URL = 'http://192.168.1.135:5000/api';
+// localhost url for mobile network
+export const API_URL = 'http://localhost:5000/api';
 // https://nuxt.com/docs/api/configuration/nuxt-config
 export default defineNuxtConfig({
   css: [
