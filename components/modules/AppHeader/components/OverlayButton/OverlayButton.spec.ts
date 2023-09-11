60e2e39a51112df1fbf6e646856c8815d84971d4
datsenkoboos <adacenkoboos@gmail.com>
2023-09-11 19:19:45 +0700

    overlay button perfect

---
+++ components/modules/AppHeader/components/OverlayButton/OverlayButton.spec.ts
@@ -0,0 +1,104 @@
+import { shallowMount } from '@vue/test-utils';
+import OverlayButton from './OverlayButton.vue';
+import { describe, it, expect, vi } from 'vitest';
+import { createTestingPinia } from '@pinia/testing';
+import { useUiStore } from '@/stores';
+
+const overlayButtonSelector = '[data-testid=overlayButton]';
+const overlayButtonIconSelector = '[data-testid=overlayButtonIcon]';
+
+describe('OverlayButton', () => {
+  beforeAll(() => {
+    vi.useFakeTimers();
+  });
+  beforeEach(() => {
+    vi.clearAllMocks();
+    vi.clearAllTimers();
+  });
+  it('User Interactions', () => {
+    it('click - should call uiStore.toggleOverlay', async () => {
+      const wrapper = shallowMount(OverlayButton, {
+        global: {
+          plugins: [createTestingPinia()],
+        },
+      });
+      const uiStore = useUiStore();
+
+      await wrapper.get(overlayButtonSelector).trigger('click');
+
+      expect(uiStore.toggleOverlay).toHaveBeenCalled();
+    });
+    it('focusout - should call uiStore.setOverlay with "false"', async () => {
+      const wrapper = shallowMount(OverlayButton, {
+        global: {
+          plugins: [createTestingPinia()],
+        },
+      });
+      const uiStore = useUiStore();
+
+      await wrapper.get(overlayButtonSelector).trigger('focusout');
+      vi.runAllTimers();
+      expect(uiStore.setOverlay).toHaveBeenCalled();
+      expect(uiStore.setOverlay).toHaveBeenCalledWith(false);
+    });
+  });
+  describe('state', () => {
+    it('uiStore.overlay - should render icon without "rotate-180" class when set to "false"', () => {
+      const wrapper = shallowMount(OverlayButton, {
+        global: {
+          plugins: [
+            createTestingPinia({
+              initialState: {
+                ui: {
+                  overlay: false,
+                },
+              },
+            }),
+          ],
+        },
+      });
+      expect(wrapper.get(overlayButtonIconSelector).classes()).not.toContain(
+        'rotate-180'
+      );
+    });
+    it('uiStore.overlay - should render icon with "rotate-180" class when set to "true"', () => {
+      const wrapper = shallowMount(OverlayButton, {
+        global: {
+          plugins: [
+            createTestingPinia({
+              initialState: {
+                ui: {
+                  overlay: true,
+                },
+              },
+            }),
+          ],
+        },
+      });
+      expect(wrapper.get(overlayButtonIconSelector).classes()).toContain(
+        'rotate-180'
+      );
+    });
+  });
+  it('snapshot - should match the snapshot', () => {
+    const wrapper = shallowMount(OverlayButton);
+    expect(wrapper.get(overlayButtonSelector)).toMatchInlineSnapshot(`
+      DOMWrapper {
+        "isDisabled": [Function],
+        "wrapperElement": <button
+          class="flex items-center gap-1"
+          data-testid="overlayButton"
+        >
+          <clientonly>
+            <profile-icon-stub />
+          </clientonly>
+          <anonymous-stub
+            class="transition-all text-[20px] rotate-180"
+            data-testid="overlayButtonIcon"
+            icon="ic:baseline-keyboard-arrow-down"
+          />
+        </button>,
+      }
+    `);
+  });
+});
--- components/modules/AppHeader/components/OverlayButton/OverlayButton.vue
+++ components/modules/AppHeader/components/OverlayButton/OverlayButton.vue
@@ -1,3 +1,28 @@
 <template>
-  <div></div>
+  <button
+    @click="uiStore.toggleOverlay"
+    class="flex items-center gap-1"
+    @focusout="closeOverlay"
+    data-testid="overlayButton"
+  >
+    <ClientOnly>
+      <ProfileIcon />
+    </ClientOnly>
+    <Icon
+      icon="ic:baseline-keyboard-arrow-down"
+      class="transition-all text-[20px]"
+      :class="uiStore.overlay ? 'rotate-180' : ''"
+      data-testid="overlayButtonIcon"
+    />
+  </button>
 </template>
+<script setup lang="ts">
+import { ProfileIcon } from '@/components/modules/AppHeader/components/ui';
+import { useUiStore } from '@/stores';
+import { Icon } from '@iconify/vue';
+const uiStore = useUiStore();
+
+function closeOverlay() {
+  setTimeout(() => uiStore.setOverlay(false), 200);
+}
+</script>
